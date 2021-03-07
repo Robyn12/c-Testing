70b25a34c287d9cc60796e1c8d9c413e0d45a5f9
Robyn12 <jenna.viktoria1@gmail.com>
2021-03-07 10:22:53 +0200

    Add files via upload

--- testipeliHack/testipeliHack.cpp
+++ testipeliHack/testipeliHack.cpp
@@ -11,7 +11,7 @@ LPVOID osoitteet[8][100];
 int scanRegion(int* index, int ind, int luku, HANDLE* phandle, DWORD start, DWORD end) {
 	int indeksi = *index;
 	int value;
-	for (DWORD i = start; i < end; i+=4) // scan through memory from start till 0x1000000 
+	for (DWORD i = start; i < end; i+=4) // scan through memory from start till end 
 	{
 		ReadProcessMemory(*phandle, (LPVOID)i, &value, sizeof(value), 0); 
 		if (value == luku) {			// if value what we read is input add it to array osoitteet
@@ -55,12 +55,12 @@ int main(int argc, char* argv[])
 	std::cout << "Give hp" << endl;
 	cin >> luku;
 	int indeksi1[8] = { 0,0,0,0 ,0,0,0,0};
-	std::thread t1(scanRegion, &indeksi1[0], 0, luku, &phandle, address, 0x200000);
-	std::thread t2(scanRegion, &indeksi1[1],1,luku, & phandle, 0x200000, 0x400000);
+	std::thread t1(scanRegion, &indeksi1[0],0, luku, &phandle, address,  0x200000);			// Start 8 threads to go through process image
+	std::thread t2(scanRegion, &indeksi1[1],1, luku, &phandle, 0x200000, 0x400000);
 	std::thread t3(scanRegion ,&indeksi1[2],2, luku, &phandle, 0x400000, 0x600000);
 	std::thread t4(scanRegion ,&indeksi1[3],3, luku, &phandle, 0x600000, 0x800000);
-	std::thread t5(scanRegion, &indeksi1[4], 4, luku, &phandle,0x800000, 0xa00000);
-	std::thread t6(scanRegion, &indeksi1[5], 5,luku, & phandle,0xa00000, 0xc00000);
+	std::thread t5(scanRegion, &indeksi1[4],4, luku, &phandle, 0x800000, 0xa00000);
+	std::thread t6(scanRegion, &indeksi1[5],5, luku, &phandle, 0xa00000, 0xc00000);
 	std::thread t7(scanRegion ,&indeksi1[6],6, luku, &phandle, 0xc00000, 0xe00000);
 	std::thread t8(scanRegion ,&indeksi1[7],7, luku, &phandle, 0xe00000, 0x1000000);
 	t1.join();
