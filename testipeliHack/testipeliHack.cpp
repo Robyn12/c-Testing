41e6ea610977d59dbf854c642398b89bbf8d62c9
Robyn12 <jenna.viktoria1@gmail.com>
2021-03-07 06:55:57 +0200

    Add files via upload

--- testipeliHack/testipeliHack.cpp
+++ testipeliHack/testipeliHack.cpp
@@ -28,17 +28,21 @@ int main(int argc, char* argv[])
 		cout << "Could not get handle!\n";
 		cin.get();
 	}
+	std::cout << pid << endl;
 	SIZE_T size;
 	size = sizeof(int);
 	address = (DWORD) phandle;
 	int indeksi = 0;
 	int luku = 0;
-	std::cout << "anna ensiluku" << endl;
+	std::cout << std::hex << address << endl;
+	std::cout << "You must hit monster once first" << endl;
+	std::cout << "Give hp" << endl;
 	cin >> luku;
-	for (DWORD i = address; i < 0x1000000; i+=4) // scan through memory from start till 0x1000000 
+	for (DWORD i = address; i < 0x2000000; i+=4) // scan through memory from start till 0x1000000 
 	{
-		ReadProcessMemory(phandle, (void*)i, &value, sizeof(value), 0); 
-		if (value == luku) {			// if value what we read is 100 add it to array osoitteet
+		ReadProcessMemory(phandle, (LPVOID)i, &value, sizeof(value), 0); 
+		if (value == luku) {			// if value what we read is input add it to array osoitteet
+			cout << std::hex << i << endl;
 			osoitteet[indeksi] = i;
 			indeksi++;
 		}
@@ -52,20 +56,20 @@ int main(int argc, char* argv[])
 	int indeksi2 = 0;
 	for(int i = 0; i < indeksi; i++) {		// scan through osoitteet to see what has changed to next value
 		
-		ReadProcessMemory(phandle, (void*)osoitteet[i], &value, sizeof(value), 0);
+		ReadProcessMemory(phandle,(LPVOID)osoitteet[i], &value, sizeof(value), 0);
 		if (value == c) {
 			cout << std::hex << osoitteet[i] << endl;
 			seuraavat[indeksi2] = osoitteet[i];	// add the address to array seuraavat
 			indeksi2++;
 		}
 	}
-	cout << "toinen scanni tehty" << endl;
+	cout << "Second scan done" << endl;
 	cout << "Next value pls" << endl;
 	cin >> c;
 	DWORD lopulliset[5];
 	int indeksi3 = 0;
 	for (int i = 0; i < indeksi2; i++) {		// scan through seuraavat
-		ReadProcessMemory(phandle, (void*)seuraavat[i], &value, sizeof(value), 0);
+		ReadProcessMemory(phandle, (LPVOID)seuraavat[i], &value, sizeof(value), 0);
 		if (value == c) {
 
 			lopulliset[indeksi3] = seuraavat[i];
@@ -83,6 +87,6 @@ int main(int argc, char* argv[])
 	}
 	std::cout << "Enter to exit..." << std::endl;
 
-	cin.get();
+	std::cin.get();
 	return 0;
 }
